8fbd0ef823a949c37e2cdd4b1cf749475e19a3ce
Kurt Schwehr <schwehr@google.com>
2026-07-05 01:36:55 -0700

    fix(init): remove obsolete __copyright__ export
    Remove __copyright__ from BitVector/__init__.py imports and __all__ since the variable was removed from BitVector.py in favor of SPDX comments.

--- BitVector/__init__.py
+++ BitVector/__init__.py
@@ -1,3 +1,3 @@
-from BitVector.BitVector import BitVector, __copyright__, __version__
+from BitVector.BitVector import BitVector, __version__
 
-__all__ = ["__version__", "__copyright__", "BitVector"]
+__all__ = ["__version__", "BitVector"]
